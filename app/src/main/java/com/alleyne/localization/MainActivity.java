a3914187d8b1ce2b1c433b3d154e0555a15146be
Keylen <Keeson@hotmail.com>
2017-09-26 17:48:10 -0400

    Finished lab5

--- app/src/main/java/com/alleyne/localization/MainActivity.java
+++ app/src/main/java/com/alleyne/localization/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +26,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //public void launchMapActivity(View v)
+    public void launchMapActivity(View v){
+        Intent i = new Intent(this, MapActivity.class);
+        logIt(getResources().getString(R.string.country));
+        i.putExtra("COUNTRY",getResources().getString(R.string.country));
+
+        startActivity(i);
+    }
+
+    public static void logIt(String msg) {
+        final String TAG = "INTCOUNTRY";
+        Log.d(TAG, msg);
+    }
 }
--- app/src/main/java/com/alleyne/localization/MapActivity.java
+++ app/src/main/java/com/alleyne/localization/MapActivity.java
@@ -1,11 +1,14 @@
 package com.alleyne.localization;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.app.SearchManager;
 
 /**
  * Created by Keylen on 2017-09-26.
@@ -19,16 +22,43 @@ public class MapActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapactivity);
         etLocal = (EditText)findViewById(R.id.local);
         errors = (TextView)findViewById(R.id.error);
         button = (Button)findViewById(R.id.button);
+        String country = getIntent().getStringExtra("COUNTRY");
+        etLocal.setText(country);
     }
 
-    protected boolean validate(View v){
+    protected boolean validate(String msg){
         if(etLocal.getText().toString().isEmpty()) {
-            errors.setText(R.string.error);
+            errors.setText(msg);
             return false;
         }
         return true;
+    }
+
+    public void showMap(View v){
+        if(validate(getString(R.string.error))) {
+            Uri geoLocation = Uri.parse("geo:0,0?q=" + Uri.encode(etLocal.getText().toString()));
+            Intent geoIntent = new Intent(Intent.ACTION_VIEW);
+
+            geoIntent.setData(geoLocation);
+            if (geoIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(geoIntent);
+            }
+        }
+    }
+
+    public void searchWeb(View view) {
+        if(validate(getString(R.string.search_request))) {
+            String query = etLocal.getText().toString();
+            MainActivity.logIt(query);
+            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            intent.putExtra(SearchManager.QUERY, query);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        }
     }
 }
