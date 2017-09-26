ed757b7edfc636c3db7e835833088d533281c876
Keylen <Keeson@hotmail.com>
2017-09-26 12:16:44 -0400

    Completed localization lab

---
+++ app/src/main/java/com/alleyne/localization/MainActivity.java
@@ -0,0 +1,29 @@
+package com.alleyne.localization;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showMap(View v){
+        String country = getResources().getString(R.string.country);
+        Uri geoLocation = Uri.parse("geo:0,0?q=" + Uri.encode(country));
+        Intent geoIntent = new Intent(Intent.ACTION_VIEW);
+
+
+        geoIntent.setData(geoLocation);
+        if (geoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(geoIntent);
+        }
+
+    }
+}
